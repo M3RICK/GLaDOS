6dcf93cf3c0cecc1f8979e068bc6dffea371786a
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-09-16 19:34:47 +0200

    [TEMP] code is unrefined, Types has temporary code and main is barely working

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
 module Main (main) where
 
-import Lib
+import System.IO
+import System.Exit
+import System.Environment (getArgs)
+import Types
+import Environment  
+import Builtins
+import qualified Parser.Core as Core
+import qualified Parser.Atomic as Atomic
+import qualified Parser.Compound as Compound
+import qualified Parser.Helpers as Helpers
+-- import evaluator
+
+-- main :: IO ()
+-- main = do
+--     args <- getArgs
+--     input <- case length args of
+--         0 -> putStrLn "damn0"
+--         2 -> putStrLn "what2"
+--         3 -> putStrLn "hunh3"
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case length args of
+        0 -> putStrLn "damn0"
+        1 -> putStrLn "wow1"
+        2 -> putStrLn "what2"
+        3 -> putStrLn "hunh3"
+        _ -> putStrLn "unhandled number of arguments"
--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,5 @@
 module Types where
+
 import qualified Data.Map as Map
 
 data LispVal
@@ -8,6 +9,23 @@ data LispVal
   | List [LispVal]
   | Function [String] LispVal Env
   | Builtin ([LispVal] -> Either String LispVal)
-  deriving (Show, Eq)
 
-type Env = Map String LispVal
+type Env = Map.Map String LispVal
+
+-- Manual Show instance
+instance Show LispVal where
+  show (Atom s) = s
+  show (Bool True) = "#t"
+  show (Bool False) = "#f"
+  show (Number n) = show n
+  show (List vals) = "(" ++ unwords (map show vals) ++ ")"
+  show (Function params _ _) = "#<procedure:" ++ unwords params ++ ">"
+  show (Builtin _) = "#<builtin>"
+
+-- Manual Eq instance
+instance Eq LispVal where
+  (Atom a) == (Atom b) = a == b
+  (Bool a) == (Bool b) = a == b
+  (Number a) == (Number b) = a == b
+  (List a) == (List b) = a == b
+  _ == _ = False  -- Functions and builtins are never equal
