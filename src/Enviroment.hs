65e3a18b3fccd125c66c926e147931863ec79454
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-09-16 16:19:21 +0200

    [FIX] corrected missplaced functions

--- src/Enviroment.hs
+++ src/Enviroment.hs
@@ -5,6 +5,9 @@ import Types
 emptyEnv :: Env
 emptyEnv = Map.empty
 
+defineVar :: String -> LispVal -> Env -> Env
+defineVar variableName value currentEnv = Map.insert variableName value currentEnv
+
 lookupVar :: String -> Env -> Either String LispVal
 lookupVar variableName currentEnv = 
     case Map.lookup variableName currentEnv of
