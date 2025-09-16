d687346bf5f3678e6d8e56f610ff72cabe4b434a
Dirty <aymeric.lamanda@epitech.eu>
2025-09-16 15:24:26 +0200

    [UPD] removed commentary

--- src/Parser/Compound.hs
+++ src/Parser/Compound.hs
@@ -6,7 +6,6 @@ import Types
 import Parser.Helpers(parens, whitespace)
 import Parser.Atomic(parseNumber, parseBool, parseSymbol)
 
--- parseExpr dans Core.hs → faut resoudre le bordel avec une injection de param plus tard.
 parseList::Parser LispVal -> Parser LispVal
 parseList parseExpr =
   List <$> parens (parseExpr `sepEndBy` whitespace)
