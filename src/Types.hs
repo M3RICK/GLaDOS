e343937bac45f7499dc9adf5fa3a4b3288237b0b
Dirty <aymeric.lamanda@epitech.eu>
2025-09-16 18:13:35 +0200

    [ADD] Types.hs / Temp patch

--- src/Types.hs
+++ src/Types.hs
@@ -8,6 +8,25 @@ data LispVal
   | List [LispVal]
   | Function [String] LispVal Env
   | Builtin ([LispVal] -> Either String LispVal)
-  deriving (Show, Eq)
+  --deriving (Show, Eq)
 
-type Env = Map String LispVal
+-- custom Show
+instance Show LispVal where
+  show (Atom s)        = s
+  show (Bool True)     = "#t"
+  show (Bool False)    = "#f"
+  show (Number n)      = show n
+  show (List xs)       = "(" ++ unwords (map show xs) ++ ")"
+  show (Function _ _ _) = "<function>"
+  show (Builtin _)      = "<builtin>"
+
+-- custom Eq
+instance Eq LispVal where
+  (Atom a)      == (Atom b)      = a == b
+  (Bool a)      == (Bool b)      = a == b
+  (Number a)    == (Number b)    = a == b
+  (List a)      == (List b)      = a == b
+  -- Functions and Builtins cannot be compared
+  _             == _             = False
+
+type Env = Map.Map String LispVal
