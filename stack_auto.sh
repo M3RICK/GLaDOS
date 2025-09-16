bb0628e49882431fb57c2a2fe0f6f9e69ce8e88b
Dirty <aymeric.lamanda@epitech.eu>
2025-09-16 17:31:46 +0200

    [ADD] stack_auto.sh

---
+++ stack_auto.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+read -p "Verbose output? (y/n): " verbose_choice
+
+if [[ "$verbose_choice" == "y" || "$verbose_choice" == "Y" ]]; then
+    verbose_flag="--verbose"
+else
+    verbose_flag=""
+fi
+
+echo "stack clean"
+stack clean $verbose_flag
+
+echo "stack build"
+stack build $verbose_flag
+
+echo "stack test"
+stack test $verbose_flag
+
+echo "Done"
+
+echo "
+⣿⣿⣿⣿⣿⣿⣿⣧⣝⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⢸⣿⣧⡹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣎⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⢸⢿⣻⡽⢷⢖⣊⣯⣭⣭⣽⣛⠿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡿⢐⣭⢖⣭⣾⠿⠛⠛⠉⠉⠙⠻⣷⡜⣿⣿⣿⣿⣿⣿⣿
+⣿⣛⣛⣛⣛⢭⡺⣿⣳⣿⣟⣵⣿⣿⣿⣿⢿⡓⠤⠀⢹⢸⣿⢫⣹⣿⣿⣿
+⣷⣝⠿⣿⣿⢻⣇⡏⣿⣿⣘⠩⠭⠁⣀⣤⡀⠀⠀⢀⡘⣾⢣⣿⡇⣿⣿⣿
+⣿⣿⣿⢈⣵⣿⣾⣷⣿⡇⠀⠀⠀⢀⣼⠿⣿⣶⣟⣵⢳⣿⢱⣿⣷⣜⢿⣿
+⣿⣿⡿⣸⣿⣿⡿⣼⣿⢻⣀⣒⣻⣭⣶⣿⣿⣿⢟⢣⡇⢫⣼⣿⣶⣿⣦⢻
+⣿⣏⡸⠏⡿⢟⣽⣿⡟⢺⡿⠿⠿⢿⣿⣛⠯⣗⣵⣿⢳⢿⣿⣿⣿⢿⣿⣿
+⣿⣿⣿⣧⡺⠟⠯⣟⡼⣿⣶⣶⣿⣿⣿⣶⣿⣿⢟⣵⣿⢸⣿⣷⣾⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣮⣭⣿⣛⣛⣯⣭⣶⣿⣿⣿⣷⣭⣛⠿⠿⢟⣼
+"
