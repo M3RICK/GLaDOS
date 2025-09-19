dedf369393b64c4dbb812ee2039ac374bd05761e
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-09-19 14:06:57 +0200

    [UPD] incorporated Aymerics more robust whitespace remover

--- src/Parser/Helpers.hs
+++ src/Parser/Helpers.hs
@@ -2,8 +2,11 @@ module Parser.Helpers (whitespace, lexeme, parens) where
 import Text.Parsec
 import Text.Parsec.String(Parser)
 
-whitespace::Parser()
-whitespace = skipMany (oneOf " \t\n\r")
+whitespace::Parser ()
+whitespace = skipMany (spaceChar <|> comment)
+  where
+    spaceChar = oneOf " \t\n\r" >> return ()
+    comment   = char ';' >> skipMany (noneOf "\n") >> return ()
 
 lexeme::Parser a -> Parser a
 lexeme p = p <* whitespace
