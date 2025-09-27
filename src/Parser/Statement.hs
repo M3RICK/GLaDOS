b29a97bc7ebe5d22e8e47f6dcda0a758e231c65b
Dirty <aymeric.lamanda@epitech.eu>
2025-09-27 19:28:45 +0200

    [ADD] Statement Parser

---
+++ src/Parser/Statement.hs
@@ -0,0 +1,75 @@
+module Parser.Statement where
+
+import Parser.Lexer
+import Parser.Expr
+import AST.AST
+
+import Text.Megaparsec
+import Control.Monad (void)
+
+
+-- If / Else
+------------------------------------------------------------
+
+pIf :: Parser Statement
+pIf = do
+  void (symbol "if")
+  cond <- parens pExpr
+  thn <- braces (many pStatement)
+  els <- optional (symbol "else" *> braces (many pStatement))
+  return (If cond thn els)
+
+-- While loop
+------------------------------------------------------------
+
+pWhile :: Parser Statement
+pWhile = do
+  void (symbol "while")
+  cond <- parens pExpr
+  body <- braces (many pStatement)
+  return (While cond body)
+
+
+-- Top-level: statement parser
+------------------------------------------------------------
+
+pStatement :: Parser Statement
+pStatement =
+      try pDecl
+  <|> try pAssign
+  <|> try pReturn
+  <|> try pIf
+  <|> try pWhile
+  <|> ExprStmt <$> (pExpr <* semi)  -- catch function calls etc.
+
+-- Declaration: int x; | int x = expr;
+------------------------------------------------------------
+
+pDecl :: Parser Statement
+pDecl = do
+  t <- pType
+  name <- pIdentifier
+  initVal <- optional (symbol "=" *> pExpr)
+  semi
+  return (Decl t name initVal)
+
+-- Assignment: x = expr;
+------------------------------------------------------------
+
+pAssign :: Parser Statement
+pAssign = do
+  name <- pIdentifier
+  void (symbol "=")
+  expr <- pExpr
+  semi
+  return (Assign name expr)
+
+-- Return: return expr;
+------------------------------------------------------------
+
+pReturn :: Parser Statement
+pReturn = do
+  void (symbol "return")
+  expr <- pExpr
+  semi
+  return (Return expr)
