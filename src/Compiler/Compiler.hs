ee36b8fa41e9a35789fa9f515e0fe2f2291a3e09
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-09-30 14:07:46 +0200

    [ADD] added makeExport and new comments for code clarity

--- src/Compiler/Compiler.hs
+++ src/Compiler/Compiler.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Compiler.Compiler where
-import AST.AST (Program)
-import qualified Language.Wasm.Structure as Wasm
 
+import AST.AST
+import qualified Language.Wasm.Structure as Wasm
+import Language.Wasm.Structure (FuncType(..), ValueType(..))
+import qualified Data.Text.Lazy as T -- Idk i think it's like a cast since i returned string but expected text
+import Numeric.Natural (Natural) -- WebAssembly needs a absolute val, no potential negatives so no ints pretty much
+
+-- Placeholder creates a empty module for now
 astToWasm :: Program -> Wasm.Module
 astToWasm program =
     Wasm.emptyModule
 
+-- Basically formats each function to wasm 
 makeFuncType :: Function -> Wasm.FuncType
 makeFuncType (Function {fType, fParams}) = 
   Wasm.FuncType paramTypes returnTypes
@@ -24,3 +31,8 @@ convertType :: Type -> Wasm.ValueType
 convertType TypeInt  = Wasm.I64
 convertType TypeBool = Wasm.I32
 convertType TypeVoid = error "TypeVoid shouldn't be converted to ValueType"
+
+-- This buids the "list" and exec order, so like index 0 is ADD
+makeExport :: Natural -> Function -> Wasm.Export
+makeExport funcIndex (Function {fName}) = 
+  Wasm.Export (T.pack fName) (Wasm.ExportFunc funcIndex)
