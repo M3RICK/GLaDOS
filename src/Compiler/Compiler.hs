6279a00d997a0695b9c81fb32000ea2805fc1c81
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-09-30 16:33:25 +0200

    [ADD] compileExpr that queues-up/finalisez data in stack INCOMPLETE

--- src/Compiler/Compiler.hs
+++ src/Compiler/Compiler.hs
@@ -18,11 +18,11 @@ makeFuncType (Function {fType, fParams}) =
   Wasm.FuncType paramTypes returnTypes
   where
     paramTypes = convertParams fParams
-    
+
     convertParams [] = []
     convertParams (Parameter pType _ : rest) = 
       convertType pType : convertParams rest
-    
+
     returnTypes = case fType of
       TypeVoid -> []
       _        -> [convertType fType]
@@ -36,3 +36,37 @@ convertType TypeVoid = error "TypeVoid shouldn't be converted to ValueType"
 makeExport :: Natural -> Function -> Wasm.Export
 makeExport funcIndex (Function {fName}) = 
   Wasm.Export (T.pack fName) (Wasm.ExportFunc funcIndex)
+
+compileFunc :: Natural -> Function -> Wasm.Function
+compileFunc typeIndex (Function {fParams, fBody}) = 
+  Wasm.Function
+    { funcType = typeIndex
+    , funcLocals = []
+    , funcBody = compileStatements fBody
+    }
+
+compileStatements :: [Statement] -> [Wasm.Instruction]
+compileStatements = concatMap compileStatement
+
+compileStatement :: Statement -> [Wasm.Instruction]
+compileStatement (Return expr) = compileExpr expr ++ [Wasm.Return]
+compileStatement _ = error "TODO: other statements"
+
+-- Execution of the expressions, removes previos info from stack like [5, 3] and replaces it with final value like [8] (if it's a addition ofc)
+compileExpr :: Expr -> [Wasm.Instruction]
+-- Adds to stack so like [] becomes [5], then if called again [5, 3]
+compileExpr (NumLit n) = [Wasm.I64Const (fromIntegral n)]
+-- Adddition
+compileExpr (BinOp Add expr1 expr2) = 
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.I64Add]
+-- Substraction
+compileExpr (BinOp Sub expr1 expr2) = 
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.I64Sub]
+-- Multiplication
+compileExpr (BinOp Mul expr1 expr2) = 
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.I64Mul]
+-- division
+compileExpr (BinOp Div expr1 expr2) = 
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.I64DivS]
+-- Dunno yet
+compileExpr _ = error "TODO: other expressions"
