ca10b6389fcd50ee9e79b99402e4b6100b83a9a0
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-09-30 13:59:31 +0200

    [ADD/FIX] CompilerSpec now supports makeExport testing/Redid indentation caused a bunch of problems

--- tests/CompilerSpec.hs
+++ tests/CompilerSpec.hs
@@ -4,26 +4,48 @@ import Test.Hspec
 import Compiler.Compiler
 import AST.AST
 import qualified Language.Wasm.Structure as Wasm
+import qualified Data.Text.Lazy as T
 
 spec :: Spec
 spec = do
-  describe "makeFuncType" $ do
-    it "converts a function with two int params returning int" $ do
-      let func = Function
-            { fType = TypeInt
-            , fName = "add"
-            , fParams = [Parameter TypeInt "a", Parameter TypeInt "b"]
-            , fBody = []
-            }
-      let result = makeFuncType func
-      result `shouldBe` Wasm.FuncType [Wasm.I64, Wasm.I64] [Wasm.I64]
+    describe "makeFuncType" $ do
+        it "converts a function with two int params returning int" $ do
+            let func = Function
+                    { fType = TypeInt
+                    , fName = "add"
+                    , fParams = [Parameter TypeInt "a", Parameter TypeInt "b"]
+                    , fBody = []
+                    }
+            let result = makeFuncType func
+            result `shouldBe` Wasm.FuncType [Wasm.I64, Wasm.I64] [Wasm.I64]
 
-    it "converts a void function with no params" $ do
-      let func = Function
-            { fType = TypeVoid
-            , fName = "doNothing"
-            , fParams = []
-            , fBody = []
-            }
-      let result = makeFuncType func
-      result `shouldBe` Wasm.FuncType [] []
+        it "converts a void function with no params" $ do
+            let func = Function
+                    { fType = TypeVoid
+                    , fName = "doNothing"
+                    , fParams = []
+                    , fBody = []
+                    }
+            let result = makeFuncType func
+            result `shouldBe` Wasm.FuncType [] []
+
+    describe "makeExport" $ do
+        it "exports add function at index 0" $ do
+            let func = Function
+                    { fType = TypeInt
+                    , fName = "add"
+                    , fParams = [Parameter TypeInt "a", Parameter TypeInt "b"]
+                    , fBody = []
+                    }
+            let result = makeExport 0 func
+            result `shouldBe` Wasm.Export (T.pack "add") (Wasm.ExportFunc 0)
+
+        it "exports multiply function at index 1" $ do
+            let func = Function
+                    { fType = TypeInt
+                    , fName = "multiply"
+                    , fParams = []
+                    , fBody = []
+                    }
+            let result = makeExport 1 func
+            result `shouldBe` Wasm.Export (T.pack "multiply") (Wasm.ExportFunc 1)
