f68372cbf437cb8ae57cfe3ad37c594adfade6f0
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-01 12:10:57 +0200

    [FIX/ADD] Incorporated the new variables from new AST (mainly location errors thx aymeric ;-;)/booleans, comparisons, and logic operations to compiler

--- src/Compiler/Compiler.hs
+++ src/Compiler/Compiler.hs
@@ -10,16 +10,16 @@ import Numeric.Natural (Natural) -- WebAssembly needs a absolute val, no potenti
 astToWasm :: Program -> Wasm.Module
 astToWasm _program = Wasm.emptyModule -- DONT FORGET TO REMOVE '_' FROM program
 
--- Basically formats each function to wasm 
+-- Basically formats each function to wasm
 makeFuncType :: Function -> Wasm.FuncType
-makeFuncType (Function {fType, fParams}) = 
+makeFuncType (Function {fType, fParams}) =
   Wasm.FuncType paramTypes returnTypes
   where
     paramTypes = convertParams fParams
 
     convertParams [] = []
-    convertParams (Parameter pType _ : rest) = 
-      convertType pType : convertParams rest
+    convertParams (Parameter paramType _ : rest) =
+      convertType paramType : convertParams rest
 
     returnTypes = case fType of
       TypeVoid -> []
@@ -53,18 +53,45 @@ compileStatement _ = error "TODO: other statements"
 -- Execution of the expressions, removes previos info from stack like [5, 3] and replaces it with final value like [8] (if it's a addition ofc)
 compileExpr :: Expr -> [Wasm.Instruction Natural]
 -- Adds to stack so like [] becomes [5], then if called again [5, 3]
-compileExpr (NumLit n) = [Wasm.I64Const (fromIntegral n)]
+compileExpr (NumLit (Located _ n)) = [Wasm.I64Const (fromIntegral n)]
+-- Boolean literals (true = 1, false = 0 in WebAssembly I32 format)
+compileExpr (BoolLit (Located _ True)) = [Wasm.I32Const 1]
+compileExpr (BoolLit (Located _ False)) = [Wasm.I32Const 0]
 -- Adddition
-compileExpr (BinOp Add expr1 expr2) = 
+compileExpr (BinOp Add (Located _ expr1) (Located _ expr2)) =
   compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
 -- Substraction
-compileExpr (BinOp Sub expr1 expr2) = 
+compileExpr (BinOp Sub (Located _ expr1) (Located _ expr2)) =
   compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.ISub]
 -- Multiplication
-compileExpr (BinOp Mul expr1 expr2) = 
+compileExpr (BinOp Mul (Located _ expr1) (Located _ expr2)) =
   compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IMul]
--- division
-compileExpr (BinOp Div expr1 expr2) = 
+-- Division
+compileExpr (BinOp Div (Located _ expr1) (Located _ expr2)) =
   compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IDivS]
--- Dunno yet
-compileExpr _ = error "TODO: other expressions"
+-- Equality comparison (returns I32: 1 if equal, 0 if not)
+compileExpr (BinOp Eq (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IEq]
+-- Not equal comparison (returns I32: 1 if different, 0 if same)
+compileExpr (BinOp Neq (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.INe]
+-- Less than comparison (signed, so negatives work correctly)
+compileExpr (BinOp Lt (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILtS]
+-- Greater than comparison
+compileExpr (BinOp Gt (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGtS]
+-- Less than or equal comparison
+compileExpr (BinOp Le (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILeS]
+-- Greater than or equal comparison
+compileExpr (BinOp Ge (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGeS]
+-- Logical AND (uses bitwise AND on I32 booleans, 1 & 1 = 1, anything else = 0)
+compileExpr (BinOp And (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
+-- Logical OR (uses bitwise OR on I32 booleans, 0 | 0 = 0, anything else = 1)
+compileExpr (BinOp Or (Located _ expr1) (Located _ expr2)) =
+  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IOr]
+-- TODO: Variable access (needs a symbol table to map variable names to local indices)
+-- TODO: Function calls (needs a function table to map function names to indices, then compile all args and emit Call instruction)
