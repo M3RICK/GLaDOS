6f5728e682a216709fc559c3c2b6dfae1cea33a3
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-01 15:34:23 +0200

    [ADD/UPD] added funcTable and incorporated it everywhere, Hspec also uptodate with all of these modifications so far

--- src/Compiler/Compiler.hs
+++ src/Compiler/Compiler.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE NamedFieldPuns #-}
-module Compiler.Compiler (astToWasm, makeFuncType, makeExport, compileFunc, compileExpr, buildVarTable, VarTable) where
+module Compiler.Compiler (astToWasm, makeFuncType, makeExport, compileFunc, compileExpr, buildVarTable, VarTable, buildFuncTable, FuncTable) where
 
 import AST.AST
 import qualified Language.Wasm.Structure as Wasm
 import qualified Data.Text.Lazy as T -- Idk i think it's like a cast since i returned string but expected text
 import Numeric.Natural (Natural) -- WebAssembly needs a absolute val, no potential negatives so no ints pretty much
 import qualified Data.Map as Map
-
--- Maps variable names to their local indices in WebAssembly
-type VarTable = Map.Map String Natural
+type VarTable = Map.Map String Natural -- Maps variable names to their local indices in WebAssembly
+type FuncTable = Map.Map String Natural -- Maps function names to their indices in WebAssembly
 
 -- TODO: Will assemble complete module with types, functions, and exports
 astToWasm :: Program -> Wasm.Module
@@ -19,6 +18,11 @@ buildVarTable :: [Parameter] -> VarTable
 buildVarTable params =
   Map.fromList $ zip (map paramName params) [0..] -- zip = ["x", "y", "z"] [0, 1, 2] -> [("x", 0), ("y", 1), ("z", 2)]
 --                                          [0..] dynamic inf list
+
+-- Creates a function table
+buildFuncTable :: [Function] -> FuncTable
+buildFuncTable funcs =
+  Map.fromList $ zip (map fName funcs) [0..] -- zip = ["main", "add", "sub"] [0, 1, 2] -> [("main", 0), ("add", 1), ("sub", 2)]
 
 -- Basically formats each function to wasm
 makeFuncType :: Function -> Wasm.FuncType
@@ -47,66 +51,70 @@ makeExport funcIndex (Function {fName}) =
   Wasm.Export (T.pack fName) (Wasm.ExportFunc funcIndex)
 
 -- Takes an (AST Function) turns it into a (Wasm.Function). typeIndex = type signature, [] = variables (will be added later), finally the wasm instructions themselfes
-compileFunc :: Natural -> Function -> Wasm.Function
-compileFunc typeIndex (Function {fParams, fBody}) =
+compileFunc :: FuncTable -> Natural -> Function -> Wasm.Function
+compileFunc funcTable typeIndex (Function {fParams, fBody}) =
   let varTable = buildVarTable fParams
-  in Wasm.Function typeIndex [] (compileStatements varTable fBody)
+  in Wasm.Function typeIndex [] (compileStatements funcTable varTable fBody)
 
 -- Plural there is a S at the end dummy
-compileStatements :: VarTable -> [Statement] -> [Wasm.Instruction Natural]
-compileStatements varTable = concatMap (compileStatement varTable)
+compileStatements :: FuncTable -> VarTable -> [Statement] -> [Wasm.Instruction Natural]
+compileStatements funcTable varTable = concatMap (compileStatement funcTable varTable)
 
 -- Singular no S
-compileStatement :: VarTable -> Statement -> [Wasm.Instruction Natural]
-compileStatement varTable (Return expr) = compileExpr varTable expr ++ [Wasm.Return]
-compileStatement _ _ = error "TODO: other statements"
+compileStatement :: FuncTable -> VarTable -> Statement -> [Wasm.Instruction Natural]
+compileStatement funcTable varTable (Return expr) = compileExpr funcTable varTable expr ++ [Wasm.Return]
+compileStatement _ _ _ = error "TODO: other statements"
 
 -- Execution of the expressions, removes previos info from stack like [5, 3] and replaces it with final value like [8] (if it's a addition ofc)
-compileExpr :: VarTable -> Expr -> [Wasm.Instruction Natural]
+compileExpr :: FuncTable -> VarTable -> Expr -> [Wasm.Instruction Natural]
 -- Adds to stack so like [] becomes [5], then if called again [5, 3]
-compileExpr _ (NumLit (Located _ n)) = [Wasm.I64Const (fromIntegral n)]
+compileExpr _ _ (NumLit (Located _ n)) = [Wasm.I64Const (fromIntegral n)]
 -- Boolean literals (true = 1, false = 0 in WebAssembly I32 format)
-compileExpr _ (BoolLit (Located _ True)) = [Wasm.I32Const 1]
-compileExpr _ (BoolLit (Located _ False)) = [Wasm.I32Const 0]
+compileExpr _ _ (BoolLit (Located _ True)) = [Wasm.I32Const 1]
+compileExpr _ _ (BoolLit (Located _ False)) = [Wasm.I32Const 0]
 -- Adddition
-compileExpr varTable (BinOp Add (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
+compileExpr funcTable varTable (BinOp Add (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
 -- Substraction
-compileExpr varTable (BinOp Sub (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.ISub]
+compileExpr funcTable varTable (BinOp Sub (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.ISub]
 -- Multiplication
-compileExpr varTable (BinOp Mul (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IMul]
+compileExpr funcTable varTable (BinOp Mul (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IMul]
 -- Division
-compileExpr varTable (BinOp Div (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IDivS]
+compileExpr funcTable varTable (BinOp Div (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IDivS]
 -- Equality comparison (returns I32: 1 if equal, 0 if not)
-compileExpr varTable (BinOp Eq (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IEq]
+compileExpr funcTable varTable (BinOp Eq (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IEq]
 -- Not equal comparison (returns I32: 1 if different, 0 if same)
-compileExpr varTable (BinOp Neq (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.INe]
+compileExpr funcTable varTable (BinOp Neq (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.INe]
 -- Less than comparison (signed, so negatives work correctly)
-compileExpr varTable (BinOp Lt (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILtS]
+compileExpr funcTable varTable (BinOp Lt (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILtS]
 -- Greater than comparison
-compileExpr varTable (BinOp Gt (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGtS]
+compileExpr funcTable varTable (BinOp Gt (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGtS]
 -- Less than or equal comparison
-compileExpr varTable (BinOp Le (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILeS]
+compileExpr funcTable varTable (BinOp Le (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILeS]
 -- Greater than or equal comparison
-compileExpr varTable (BinOp Ge (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGeS]
+compileExpr funcTable varTable (BinOp Ge (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGeS]
 -- Logical AND (in c it's &&)
-compileExpr varTable (BinOp And (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
+compileExpr funcTable varTable (BinOp And (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
 -- Logical OR (in c it's |)
-compileExpr varTable (BinOp Or (Located _ expr1) (Located _ expr2)) =
-  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IOr]
+compileExpr funcTable varTable (BinOp Or (Located _ expr1) (Located _ expr2)) =
+  compileExpr funcTable varTable expr1 ++ compileExpr funcTable varTable expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IOr]
 -- Variable access (looks up variable name in table and emits GetLocal instruction to read its value from the stack)
-compileExpr varTable (Var (Located _ name)) =
+compileExpr _ varTable (Var (Located _ name)) =
   case Map.lookup name varTable of
     Just idx -> [Wasm.GetLocal idx]
     Nothing  -> error $ "Variable not found: " ++ name
--- TODO: Function calls (needs a function table to map function names to indices, then compile all args and emit Call instruction)
+-- Function calls (compile all arguments and emit Call instruction with function index from table)
+compileExpr funcTable varTable (Call (Located _ funcName) args) =
+  case Map.lookup funcName funcTable of
+    Just funcIdx -> concatMap (compileExpr funcTable varTable) args ++ [Wasm.Call funcIdx]
+    Nothing -> error $ "Function not found: " ++ funcName
--- tests/CompilerSpec.hs
+++ tests/CompilerSpec.hs
@@ -16,6 +16,10 @@ loc = Located (initialPos "<test>")
 emptyVarTable :: VarTable
 emptyVarTable = Map.empty
 
+-- Empty FuncTable for tests that don't use functions
+emptyFuncTable :: FuncTable
+emptyFuncTable = Map.empty
+
 spec :: Spec
 spec = do
 
@@ -81,7 +85,7 @@ spec = do
                     , fParams = []
                     , fBody = [Return (BinOp Add (loc (NumLit (loc 5))) (loc (NumLit (loc 3))))]
                     }
-            let Wasm.Function _ _ resultBody = compileFunc 0 func
+            let Wasm.Function _ _ resultBody = compileFunc emptyFuncTable 0 func
             resultBody `shouldBe` [Wasm.I64Const 5, Wasm.I64Const 3, Wasm.IBinOp Wasm.BS64 Wasm.IAdd, Wasm.Return]
 
 -- ============================================================================
@@ -90,81 +94,81 @@ spec = do
 
     describe "compileExpr - Literals" $ do
         it "compiles integer literal" $ do
-            let result = compileExpr emptyVarTable (NumLit (loc 42))
+            let result = compileExpr emptyFuncTable emptyVarTable (NumLit (loc 42))
             result `shouldBe` [Wasm.I64Const 42]
 
         it "compiles negative integer literal" $ do
-            let result = compileExpr emptyVarTable (NumLit (loc (-17)))
+            let result = compileExpr emptyFuncTable emptyVarTable (NumLit (loc (-17)))
             result `shouldBe` [Wasm.I64Const (-17)]
 
         it "compiles boolean literal true" $ do
-            let result = compileExpr emptyVarTable (BoolLit (loc True))
+            let result = compileExpr emptyFuncTable emptyVarTable (BoolLit (loc True))
             result `shouldBe` [Wasm.I32Const 1]
 
         it "compiles boolean literal false" $ do
-            let result = compileExpr emptyVarTable (BoolLit (loc False))
+            let result = compileExpr emptyFuncTable emptyVarTable (BoolLit (loc False))
             result `shouldBe` [Wasm.I32Const 0]
 
 
     describe "compileExpr - Arithmetic Operations" $ do
         it "compiles addition" $ do
-            let result = compileExpr emptyVarTable (BinOp Add (loc (NumLit (loc 10))) (loc (NumLit (loc 20))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Add (loc (NumLit (loc 10))) (loc (NumLit (loc 20))))
             result `shouldBe` [Wasm.I64Const 10, Wasm.I64Const 20, Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
 
         it "compiles subtraction" $ do
-            let result = compileExpr emptyVarTable (BinOp Sub (loc (NumLit (loc 50))) (loc (NumLit (loc 30))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Sub (loc (NumLit (loc 50))) (loc (NumLit (loc 30))))
             result `shouldBe` [Wasm.I64Const 50, Wasm.I64Const 30, Wasm.IBinOp Wasm.BS64 Wasm.ISub]
 
         it "compiles multiplication" $ do
-            let result = compileExpr emptyVarTable (BinOp Mul (loc (NumLit (loc 7))) (loc (NumLit (loc 6))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Mul (loc (NumLit (loc 7))) (loc (NumLit (loc 6))))
             result `shouldBe` [Wasm.I64Const 7, Wasm.I64Const 6, Wasm.IBinOp Wasm.BS64 Wasm.IMul]
 
         it "compiles division" $ do
-            let result = compileExpr emptyVarTable (BinOp Div (loc (NumLit (loc 100))) (loc (NumLit (loc 5))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Div (loc (NumLit (loc 100))) (loc (NumLit (loc 5))))
             result `shouldBe` [Wasm.I64Const 100, Wasm.I64Const 5, Wasm.IBinOp Wasm.BS64 Wasm.IDivS]
 
 
     describe "compileExpr - Comparison Operations" $ do
         it "compiles equality comparison" $ do
-            let result = compileExpr emptyVarTable (BinOp Eq (loc (NumLit (loc 5))) (loc (NumLit (loc 5))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Eq (loc (NumLit (loc 5))) (loc (NumLit (loc 5))))
             result `shouldBe` [Wasm.I64Const 5, Wasm.I64Const 5, Wasm.IRelOp Wasm.BS64 Wasm.IEq]
 
         it "compiles not equal comparison" $ do
-            let result = compileExpr emptyVarTable (BinOp Neq (loc (NumLit (loc 3))) (loc (NumLit (loc 7))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Neq (loc (NumLit (loc 3))) (loc (NumLit (loc 7))))
             result `shouldBe` [Wasm.I64Const 3, Wasm.I64Const 7, Wasm.IRelOp Wasm.BS64 Wasm.INe]
 
         it "compiles less than comparison" $ do
-            let result = compileExpr emptyVarTable (BinOp Lt (loc (NumLit (loc 2))) (loc (NumLit (loc 8))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Lt (loc (NumLit (loc 2))) (loc (NumLit (loc 8))))
             result `shouldBe` [Wasm.I64Const 2, Wasm.I64Const 8, Wasm.IRelOp Wasm.BS64 Wasm.ILtS]
 
         it "compiles greater than comparison" $ do
-            let result = compileExpr emptyVarTable (BinOp Gt (loc (NumLit (loc 15))) (loc (NumLit (loc 10))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Gt (loc (NumLit (loc 15))) (loc (NumLit (loc 10))))
             result `shouldBe` [Wasm.I64Const 15, Wasm.I64Const 10, Wasm.IRelOp Wasm.BS64 Wasm.IGtS]
 
         it "compiles less than or equal comparison" $ do
-            let result = compileExpr emptyVarTable (BinOp Le (loc (NumLit (loc 5))) (loc (NumLit (loc 5))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Le (loc (NumLit (loc 5))) (loc (NumLit (loc 5))))
             result `shouldBe` [Wasm.I64Const 5, Wasm.I64Const 5, Wasm.IRelOp Wasm.BS64 Wasm.ILeS]
 
         it "compiles greater than or equal comparison" $ do
-            let result = compileExpr emptyVarTable (BinOp Ge (loc (NumLit (loc 12))) (loc (NumLit (loc 8))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Ge (loc (NumLit (loc 12))) (loc (NumLit (loc 8))))
             result `shouldBe` [Wasm.I64Const 12, Wasm.I64Const 8, Wasm.IRelOp Wasm.BS64 Wasm.IGeS]
 
 
     describe "compileExpr - Logic Operations" $ do
         it "compiles logical AND with two true values" $ do
-            let result = compileExpr emptyVarTable (BinOp And (loc (BoolLit (loc True))) (loc (BoolLit (loc True))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp And (loc (BoolLit (loc True))) (loc (BoolLit (loc True))))
             result `shouldBe` [Wasm.I32Const 1, Wasm.I32Const 1, Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
 
         it "compiles logical AND with true and false" $ do
-            let result = compileExpr emptyVarTable (BinOp And (loc (BoolLit (loc True))) (loc (BoolLit (loc False))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp And (loc (BoolLit (loc True))) (loc (BoolLit (loc False))))
             result `shouldBe` [Wasm.I32Const 1, Wasm.I32Const 0, Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
 
         it "compiles logical OR with two false values" $ do
-            let result = compileExpr emptyVarTable (BinOp Or (loc (BoolLit (loc False))) (loc (BoolLit (loc False))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Or (loc (BoolLit (loc False))) (loc (BoolLit (loc False))))
             result `shouldBe` [Wasm.I32Const 0, Wasm.I32Const 0, Wasm.IBinOp Wasm.BS32 Wasm.IOr]
 
         it "compiles logical OR with false and true" $ do
-            let result = compileExpr emptyVarTable (BinOp Or (loc (BoolLit (loc False))) (loc (BoolLit (loc True))))
+            let result = compileExpr emptyFuncTable emptyVarTable (BinOp Or (loc (BoolLit (loc False))) (loc (BoolLit (loc True))))
             result `shouldBe` [Wasm.I32Const 0, Wasm.I32Const 1, Wasm.IBinOp Wasm.BS32 Wasm.IOr]
 
 
@@ -173,7 +177,7 @@ spec = do
             let expr = BinOp Mul
                         (loc (BinOp Add (loc (NumLit (loc 2))) (loc (NumLit (loc 3)))))
                         (loc (NumLit (loc 4)))
-            let result = compileExpr emptyVarTable expr
+            let result = compileExpr emptyFuncTable emptyVarTable expr
             result `shouldBe` [ Wasm.I64Const 2, Wasm.I64Const 3, Wasm.IBinOp Wasm.BS64 Wasm.IAdd
                               , Wasm.I64Const 4, Wasm.IBinOp Wasm.BS64 Wasm.IMul ]
 
@@ -181,7 +185,7 @@ spec = do
             let expr = BinOp Lt
                         (loc (BinOp Add (loc (NumLit (loc 10))) (loc (NumLit (loc 5)))))
                         (loc (BinOp Sub (loc (NumLit (loc 20))) (loc (NumLit (loc 3)))))
-            let result = compileExpr emptyVarTable expr
+            let result = compileExpr emptyFuncTable emptyVarTable expr
             result `shouldBe` [ Wasm.I64Const 10, Wasm.I64Const 5, Wasm.IBinOp Wasm.BS64 Wasm.IAdd
                               , Wasm.I64Const 20, Wasm.I64Const 3, Wasm.IBinOp Wasm.BS64 Wasm.ISub
                               , Wasm.IRelOp Wasm.BS64 Wasm.ILtS ]
@@ -194,17 +198,17 @@ spec = do
     describe "compileExpr - Variable Access" $ do
         it "compiles variable access for first parameter" $ do
             let varTable = Map.fromList [("x", 0)]
-            let result = compileExpr varTable (Var (loc "x"))
+            let result = compileExpr emptyFuncTable varTable (Var (loc "x"))
             result `shouldBe` [Wasm.GetLocal 0]
 
         it "compiles variable access for second parameter" $ do
             let varTable = Map.fromList [("x", 0), ("y", 1)]
-            let result = compileExpr varTable (Var (loc "y"))
+            let result = compileExpr emptyFuncTable varTable (Var (loc "y"))
             result `shouldBe` [Wasm.GetLocal 1]
 
         it "compiles expression using variables (x + y)" $ do
             let varTable = Map.fromList [("x", 0), ("y", 1)]
-            let result = compileExpr varTable (BinOp Add (loc (Var (loc "x"))) (loc (Var (loc "y"))))
+            let result = compileExpr emptyFuncTable varTable (BinOp Add (loc (Var (loc "x"))) (loc (Var (loc "y"))))
             result `shouldBe` [Wasm.GetLocal 0, Wasm.GetLocal 1, Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
 
 
@@ -224,3 +228,65 @@ spec = do
         it "builds table from multiple parameters" $ do
             let result = buildVarTable [Parameter TypeInt "a", Parameter TypeInt "b", Parameter TypeInt "c"]
             result `shouldBe` Map.fromList [("a", 0), ("b", 1), ("c", 2)]
+
+
+-- ============================================================================
+-- buildFuncTable Tests
+-- ============================================================================
+
+    describe "buildFuncTable" $ do
+        it "builds empty table from no functions" $ do
+            let result = buildFuncTable []
+            result `shouldBe` Map.empty
+
+        it "builds table from single function" $ do
+            let func = Function TypeInt "add" [] []
+            let result = buildFuncTable [func]
+            result `shouldBe` Map.fromList [("add", 0)]
+
+        it "builds table from multiple functions" $ do
+            let func1 = Function TypeInt "main" [] []
+            let func2 = Function TypeInt "add" [] []
+            let func3 = Function TypeVoid "helper" [] []
+            let result = buildFuncTable [func1, func2, func3]
+            result `shouldBe` Map.fromList [("main", 0), ("add", 1), ("helper", 2)]
+
+
+-- ============================================================================
+-- Function Call Tests
+-- ============================================================================
+
+    describe "compileExpr - Function Calls" $ do
+        it "compiles function call with no arguments" $ do
+            let funcTable = Map.fromList [("getAnswer", 0)]
+            let result = compileExpr funcTable emptyVarTable (Call (loc "getAnswer") [])
+            result `shouldBe` [Wasm.Call 0]
+
+        it "compiles function call with one argument" $ do
+            let funcTable = Map.fromList [("double", 1)]
+            let result = compileExpr funcTable emptyVarTable (Call (loc "double") [NumLit (loc 21)])
+            result `shouldBe` [Wasm.I64Const 21, Wasm.Call 1]
+
+        it "compiles function call with multiple arguments" $ do
+            let funcTable = Map.fromList [("add", 2)]
+            let result = compileExpr funcTable emptyVarTable (Call (loc "add") [NumLit (loc 10), NumLit (loc 20)])
+            result `shouldBe` [Wasm.I64Const 10, Wasm.I64Const 20, Wasm.Call 2]
+
+        it "compiles function call with expression arguments" $ do
+            let funcTable = Map.fromList [("multiply", 0)]
+            let arg1 = BinOp Add (loc (NumLit (loc 2))) (loc (NumLit (loc 3)))
+            let arg2 = NumLit (loc 4)
+            let result = compileExpr funcTable emptyVarTable (Call (loc "multiply") [arg1, arg2])
+            result `shouldBe` [Wasm.I64Const 2, Wasm.I64Const 3, Wasm.IBinOp Wasm.BS64 Wasm.IAdd, Wasm.I64Const 4, Wasm.Call 0]
+
+        it "compiles function call with variable arguments" $ do
+            let funcTable = Map.fromList [("sum", 3)]
+            let varTable = Map.fromList [("x", 0), ("y", 1)]
+            let result = compileExpr funcTable varTable (Call (loc "sum") [Var (loc "x"), Var (loc "y")])
+            result `shouldBe` [Wasm.GetLocal 0, Wasm.GetLocal 1, Wasm.Call 3]
+
+        it "compiles nested function calls" $ do
+            let funcTable = Map.fromList [("inner", 0), ("outer", 1)]
+            let innerCall = Call (loc "inner") [NumLit (loc 5)]
+            let result = compileExpr funcTable emptyVarTable (Call (loc "outer") [innerCall])
+            result `shouldBe` [Wasm.I64Const 5, Wasm.Call 0, Wasm.Call 1]
