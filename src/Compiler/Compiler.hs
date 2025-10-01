ed00a61e6e24201724ed7436cbd694cfd62ea959
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-01 13:54:28 +0200

    [UPD/ADD] modified most of file to incorporate the new table (varTable)/variable access support with VarTable

--- src/Compiler/Compiler.hs
+++ src/Compiler/Compiler.hs
@@ -48,60 +48,65 @@ makeExport funcIndex (Function {fName}) =
 
 -- Takes an (AST Function) turns it into a (Wasm.Function). typeIndex = type signature, [] = variables (will be added later), finally the wasm instructions themselfes
 compileFunc :: Natural -> Function -> Wasm.Function
-compileFunc typeIndex (Function {fBody}) =
-  Wasm.Function typeIndex [] (compileStatements fBody)
+compileFunc typeIndex (Function {fParams, fBody}) =
+  let varTable = buildVarTable fParams
+  in Wasm.Function typeIndex [] (compileStatements varTable fBody)
 
 -- Plural there is a S at the end dummy
-compileStatements :: [Statement] -> [Wasm.Instruction Natural]
-compileStatements = concatMap compileStatement
+compileStatements :: VarTable -> [Statement] -> [Wasm.Instruction Natural]
+compileStatements varTable = concatMap (compileStatement varTable)
 
 -- Singular no S
-compileStatement :: Statement -> [Wasm.Instruction Natural]
-compileStatement (Return expr) = compileExpr expr ++ [Wasm.Return]
-compileStatement _ = error "TODO: other statements"
+compileStatement :: VarTable -> Statement -> [Wasm.Instruction Natural]
+compileStatement varTable (Return expr) = compileExpr varTable expr ++ [Wasm.Return]
+compileStatement _ _ = error "TODO: other statements"
 
 -- Execution of the expressions, removes previos info from stack like [5, 3] and replaces it with final value like [8] (if it's a addition ofc)
-compileExpr :: Expr -> [Wasm.Instruction Natural]
+compileExpr :: VarTable -> Expr -> [Wasm.Instruction Natural]
 -- Adds to stack so like [] becomes [5], then if called again [5, 3]
-compileExpr (NumLit (Located _ n)) = [Wasm.I64Const (fromIntegral n)]
+compileExpr _ (NumLit (Located _ n)) = [Wasm.I64Const (fromIntegral n)]
 -- Boolean literals (true = 1, false = 0 in WebAssembly I32 format)
-compileExpr (BoolLit (Located _ True)) = [Wasm.I32Const 1]
-compileExpr (BoolLit (Located _ False)) = [Wasm.I32Const 0]
+compileExpr _ (BoolLit (Located _ True)) = [Wasm.I32Const 1]
+compileExpr _ (BoolLit (Located _ False)) = [Wasm.I32Const 0]
 -- Adddition
-compileExpr (BinOp Add (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
+compileExpr varTable (BinOp Add (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IAdd]
 -- Substraction
-compileExpr (BinOp Sub (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.ISub]
+compileExpr varTable (BinOp Sub (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.ISub]
 -- Multiplication
-compileExpr (BinOp Mul (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IMul]
+compileExpr varTable (BinOp Mul (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IMul]
 -- Division
-compileExpr (BinOp Div (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IDivS]
+compileExpr varTable (BinOp Div (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS64 Wasm.IDivS]
 -- Equality comparison (returns I32: 1 if equal, 0 if not)
-compileExpr (BinOp Eq (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IEq]
+compileExpr varTable (BinOp Eq (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IEq]
 -- Not equal comparison (returns I32: 1 if different, 0 if same)
-compileExpr (BinOp Neq (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.INe]
+compileExpr varTable (BinOp Neq (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.INe]
 -- Less than comparison (signed, so negatives work correctly)
-compileExpr (BinOp Lt (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILtS]
+compileExpr varTable (BinOp Lt (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILtS]
 -- Greater than comparison
-compileExpr (BinOp Gt (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGtS]
+compileExpr varTable (BinOp Gt (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGtS]
 -- Less than or equal comparison
-compileExpr (BinOp Le (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILeS]
+compileExpr varTable (BinOp Le (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.ILeS]
 -- Greater than or equal comparison
-compileExpr (BinOp Ge (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGeS]
--- Logical AND (uses bitwise AND on I32 booleans, 1 & 1 = 1, anything else = 0)
-compileExpr (BinOp And (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
--- Logical OR (uses bitwise OR on I32 booleans, 0 | 0 = 0, anything else = 1)
-compileExpr (BinOp Or (Located _ expr1) (Located _ expr2)) =
-  compileExpr expr1 ++ compileExpr expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IOr]
--- TODO: Variable access (needs a symbol table to map variable names to local indices)
+compileExpr varTable (BinOp Ge (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IRelOp Wasm.BS64 Wasm.IGeS]
+-- Logical AND (in c it's &&)
+compileExpr varTable (BinOp And (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IAnd]
+-- Logical OR (in c it's |)
+compileExpr varTable (BinOp Or (Located _ expr1) (Located _ expr2)) =
+  compileExpr varTable expr1 ++ compileExpr varTable expr2 ++ [Wasm.IBinOp Wasm.BS32 Wasm.IOr]
+-- Variable access (looks up variable name in table and emits GetLocal instruction to read its value from the stack)
+compileExpr varTable (Var (Located _ name)) =
+  case Map.lookup name varTable of
+    Just idx -> [Wasm.GetLocal idx]
+    Nothing  -> error $ "Variable not found: " ++ name
 -- TODO: Function calls (needs a function table to map function names to indices, then compile all args and emit Call instruction)
