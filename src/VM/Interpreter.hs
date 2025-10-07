97431353b74b6bd4ac1b6e38053779737ba899b6
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-07 15:00:33 +0200

    [ADD] getlocal, setlocal, popint, popbool, binaryintop, binaryboolop and compareints. Should be helpers done

--- src/VM/Interpreter.hs
+++ src/VM/Interpreter.hs
@@ -1,7 +1,7 @@
 module VM.Interpreter where
 
 import IR.Types
-import Control.Monad (when)
+
 type VMError = String -- error msg string
 type VMResult a = Either VMError a
 
@@ -31,3 +31,61 @@ pop :: VMState -> VMResult (Value, VMState)
 pop state = case stack state of
     []     -> Left "Stack underflow: tried to pop from empty stack"
     (x:xs) -> Right (x, state {stack = xs})
+
+-- Get a local variable by index
+getLocal :: Int -> VMState -> VMResult Value
+getLocal idx state
+    | idx < 0 = Left $ "Invalid local index: " ++ show idx ++ " (negative)"
+    | idx >= length (locals state) = Left $ "Local index out of bounds: " ++ show idx ++ " (max: " ++ show (length (locals state) - 1) ++ ")"
+    | otherwise = Right (locals state !! idx)
+
+-- Set a local variable by index
+setLocal :: Int -> Value -> VMState -> VMResult VMState
+setLocal idx val state
+    | idx < 0 = Left $ "Invalid local index: " ++ show idx ++ " (negative)"
+    | idx >= length (locals state) = Left $ "Local index out of bounds: " ++ show idx ++ " (max: " ++ show (length (locals state) - 1) ++ ")"
+    | otherwise = Right state { locals = updateAt idx val (locals state) }
+    where
+        updateAt :: Int -> a -> [a] -> [a]
+        updateAt n item ls = take n ls ++ [item] ++ drop (n + 1) ls
+
+-- Pop and type-check for Int
+popInt :: VMState -> VMResult (Int, VMState)
+popInt state = case pop state of
+    Right (VInt n, newState) -> Right (n, newState)
+    Right (VBool _, _) -> Left "Type error: expected Int, got Bool"
+    Left err -> Left err
+
+-- Pop and type-check for Bool
+popBool :: VMState -> VMResult (Bool, VMState)
+popBool state = case pop state of
+    Right (VBool b, newState) -> Right (b, newState)
+    Right (VInt _, _) -> Left "Type error: expected Bool, got Int"
+    Left err -> Left err
+
+-- BinOp does as follows pop 2 ints, apply op, push result
+binaryIntOp :: (Int -> Int -> Int) -> VMState -> VMResult VMState
+binaryIntOp op state =
+    case popInt state of
+        Left err -> Left err
+        Right (b, state1) -> case popInt state1 of
+            Left err -> Left err
+            Right (a, state2) -> Right (push (VInt (a `op` b)) state2)
+
+-- Same execution principle as above
+compareInts :: (Int -> Int -> Bool) -> VMState -> VMResult VMState
+compareInts cmp state =
+    case popInt state of
+        Left err -> Left err
+        Right (b, state1) -> case popInt state1 of
+            Left err -> Left err
+            Right (a, state2) -> Right (push (VBool (a `cmp` b)) state2)
+
+-- Just like BinOp
+binaryBoolOp :: (Bool -> Bool -> Bool) -> VMState -> VMResult VMState
+binaryBoolOp op state =
+    case popBool state of
+        Left err -> Left err
+        Right (b, state1) -> case popBool state1 of
+            Left err -> Left err
+            Right (a, state2) -> Right (push (VBool (a `op` b)) state2)
