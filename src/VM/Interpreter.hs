74b856b20e83e29d8a3b0db0e01b04d55d9f1666
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-07 16:15:57 +0200

    [UPD] seperated VM files once more for readability

--- src/VM/Interpreter.hs
+++ src/VM/Interpreter.hs
@@ -2,78 +2,9 @@ module VM.Interpreter where
 
 import IR.Types
 import VM.HelperFunc
+import VM.InstructionHandlers
 
--- Stack management
-executeStackOp :: Instruction -> VMState -> VMResult VMState
-executeStackOp instr state = case instr of
-    PushInt n -> Right (push (VInt n) state)
-    PushBool b -> Right (push (VBool b) state)
-    Pop -> case pop state of
-        Right (_, newState) -> Right newState
-        Left err -> Left err
-    _ -> Left "Internal error: not a stack operation"
-
--- Local var interactuions
-executeVarOp :: Instruction -> VMState -> VMResult VMState
-executeVarOp instr state = case instr of
-    GetLocal idx -> case getLocal idx state of
-        Right val -> Right (push val state)
-        Left err -> Left err
-    SetLocal idx -> case pop state of
-        Right (val, newState) -> setLocal idx val newState
-        Left err -> Left err
-    _ -> Left "Internal error: not a variable operation"
-
--- Math
-executeArithOp :: Instruction -> VMState -> VMResult VMState
-executeArithOp instr state = case instr of
-    AddInt -> binaryIntOp (+) state
-    SubInt -> binaryIntOp (-) state
-    MulInt -> binaryIntOp (*) state
-    DivInt -> case popInt state of
-        Left err -> Left err
-        Right (b, state1) ->
-            if b == 0
-            then Left "Runtime error: Division by zero"
-            else case popInt state1 of
-                Left err -> Left err
-                Right (a, state2) -> Right (push (VInt (a `div` b)) state2)
-    _ -> Left "Internal error: not an arithmetic operation"
-
--- Comparaison
-executeCompOp :: Instruction -> VMState -> VMResult VMState
-executeCompOp instr state = case instr of
-    EqInt -> compareInts (==) state
-    NeqInt -> compareInts (/=) state
-    LtInt -> compareInts (<) state
-    GtInt -> compareInts (>) state
-    LeInt -> compareInts (<=) state
-    GeInt -> compareInts (>=) state
-    _ -> Left "Internal error: not a comparison operation"
-
--- &&/||
-executeLogicOp :: Instruction -> VMState -> VMResult VMState
-executeLogicOp instr state = case instr of
-    AndBool -> binaryBoolOp (&&) state
-    OrBool -> binaryBoolOp (||) state
-    _ -> Left "Internal error: not a logical operation"
-
--- Flow op
-executeControlFlow :: Instruction -> VMState -> VMResult VMState
-executeControlFlow instr state = case instr of
-    Jump addr -> Right (state {pc = addr})
-    JumpIfFalse addr -> case popBool state of
-        Left err -> Left err
-        Right (condition, newState) ->
-            if condition
-            then Right newState
-            else Right (newState {pc = addr})
-    Call _funcIdx -> Left "Call instruction not yet implemented"
-    Return -> Left "Return instruction not yet implemented"
-    Halt -> Right state
-    _ -> Left "Internal error: not a control flow operation"
-
--- The Pâté Carrefour
+-- The Pâté Carrefour (Main dispatcher)
 executeInstruction :: Instruction -> VMState -> VMResult VMState
 executeInstruction instr state = case instr of
     PushInt _ -> executeStackOp instr state
