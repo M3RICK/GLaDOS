ada1a7491aafadf4851daf9726c11c3828695d1c
MrKiwistiti <sven5reichert@gmail.com>
2025-10-07 17:23:50 +0200

    [ADD] simple main (part one) waiting for progress

--- app/Main.hs
+++ app/Main.hs
@@ -7,34 +7,18 @@
 
 module Main (main) where
 
-import System.IO (hFlush, stdout)
 import Parser (parseProgram, AST(..))
 import Compiler (compileAndRun)
 
--- | Fonction principale simple
 main :: IO ()
 main = do
-    putStr "GLaDOS> "
-    hFlush stdout
     input <- getLine
-    
-    if null input
-        then putStrLn "Empty input"
-        else processInput input
-
--- | Traite l'entrée utilisateur
-processInput :: String -> IO ()
-processInput input = do
     case parseProgram input of
-        Left parseErr -> putStrLn $ "Parse error: " ++ parseErr
-        Right ast -> executeAST ast
-
--- | Exécute l'AST
-executeAST :: AST -> IO ()
-executeAST (ASTInt n) = print n
-executeAST (ASTBool b) = print b
-executeAST (ASTExpression expr) = do
-    case compileAndRun expr of
-        Left err -> putStrLn $ "Error: " ++ show err
-        Right result -> print result
-executeAST (ASTProgram _) = putStrLn "Program execution not implemented yet"
+        Left _ -> return ()
+        Right ast -> case ast of
+            ASTInt n -> print n
+            ASTBool b -> print b
+            ASTExpression expr -> case compileAndRun expr of
+                Right result -> print result
+                Left _ -> return ()
+            _ -> return ()
