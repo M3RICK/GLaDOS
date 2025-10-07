99f8cd2d817d490c0c33a3577875ccc492630617
Peres-Aurelien <aurelien.peres@epitech.eu>
2025-10-07 12:13:24 +0200

    [DEL] useless file

--- src/AST/AST.hs
+++ src/AST/AST.hs
@@ -1,61 +1,126 @@
--- src/AST/AST.hs
 module AST.AST where
 
 import Text.Megaparsec.Pos (SourcePos)
 
--- | A value tagged with its source position (for nice error messages).
-data Located a = Located
-  { pos   :: SourcePos
-  , value :: a
-  } deriving (Eq, Show)
+-- | A whole program = list of functions
+data Program = Program [Function]
+  deriving (Show, Eq)
 
--- | Language types (we’ll extend later with Float/Char/String).
+-- | Function declaration
+data Function = Function
+  { fType       :: Type
+  , fName       :: String
+  , fParams     :: [Parameter]
+  , fBody       :: [Statement]
+  }
+  deriving (Show, Eq) -- temporaire va falloir creer un PrintAst :: AST -> String et aussi utiliser SourcePos de Megaparsec pour des beaux msg d erreurs
+
+-- | Function parameter
+data Parameter = Parameter
+  { paramType :: Type
+  , paramName :: String
+  }
+  deriving (Show, Eq)
+
+-- | Statements inside a function
+data Statement
+  = Decl Type String (Maybe Expr)           -- int x; or int x = expr;
+  | Assign String Expr                      -- x = expr;
+  | If Expr [Statement] (Maybe [Statement]) -- if (...) { ... } else { ... }
+  | While Expr [Statement]                  -- while (...) { ... }
+  | Return Expr                             -- return expr;
+  | ExprStmt Expr                           -- e.g. function call as statement
+  deriving (Show, Eq)
+
+-- | Expressions
+data Expr
+  = BoolLit (Located Bool)
+  | NumLit (Located Int)
+  | Var (Located String)
+  | BinOp Op (Located Expr) (Located Expr)  -- Keep position of whole expression
+  | Call (Located String) [Expr]
+  deriving (Show, Eq)
+
+-- | Supported types
 data Type
   = TypeInt
   | TypeBool
   | TypeVoid
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
--- | Binary operators.
+-- | Operators
 data Op
   = Add | Sub | Mul | Div
-  | Eq  | Neq | Lt | Gt | Le | Ge
+  | Eq | Neq | Lt | Gt | Le | Ge
   | And | Or
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
--- | Expressions.
-data Expr
-  = BoolLit (Located Bool)
-  | NumLit  (Located Int)
-  | Var     (Located String)
-  | BinOp   Op (Located Expr) (Located Expr)
-  | Call    (Located String) [Expr]
-  deriving (Eq, Show)
+-- Security
+data Located a
+    = Located {
+        pos :: SourcePos,
+        value :: a
+    } deriving (Show, Eq)
+module AST.AST where
 
--- | Statements.
-data Statement
-  = Decl   Type String (Maybe Expr)             -- int x; / int x = expr;
-  | Assign String Expr                          -- x = expr;
-  | If     Expr [Statement] (Maybe [Statement]) -- if (...) { ... } else { ... }
-  | While  Expr [Statement]                     -- while (...) { ... }
-  | Return Expr                                 -- return expr;
-  | ExprStmt Expr                               -- e.g. a function call as stmt
-  deriving (Eq, Show)
+import Text.Megaparsec.Pos (SourcePos)
 
--- | Function parameter.
+-- | A whole program = list of functions
+data Program = Program [Function]
+  deriving (Show, Eq)
+
+-- | Function declaration
+data Function = Function
+  { fType       :: Type
+  , fName       :: String
+  , fParams     :: [Parameter]
+  , fBody       :: [Statement]
+  }
+  deriving (Show, Eq) -- temporaire va falloir creer un PrintAst :: AST -> String et aussi utiliser SourcePos de Megaparsec pour des beaux msg d erreurs
+
+-- | Function parameter
 data Parameter = Parameter
   { paramType :: Type
   , paramName :: String
-  } deriving (Eq, Show)
+  }
+  deriving (Show, Eq)
 
--- | Function definition.
-data Function = Function
-  { fType   :: Type
-  , fName   :: String
-  , fParams :: [Parameter]
-  , fBody   :: [Statement]
-  } deriving (Eq, Show)
+-- | Statements inside a function
+data Statement
+  = Decl Type String (Maybe Expr)           -- int x; or int x = expr;
+  | Assign String Expr                      -- x = expr;
+  | If Expr [Statement] (Maybe [Statement]) -- if (...) { ... } else { ... }
+  | While Expr [Statement]                  -- while (...) { ... }
+  | Return Expr                             -- return expr;
+  | ExprStmt Expr                           -- e.g. function call as statement
+  deriving (Show, Eq)
 
--- | Program = list of functions.
-newtype Program = Program [Function]
-  deriving (Eq, Show)
+-- | Expressions
+data Expr
+  = BoolLit (Located Bool)
+  | NumLit (Located Int)
+  | Var (Located String)
+  | BinOp Op (Located Expr) (Located Expr)  -- Keep position of whole expression
+  | Call (Located String) [Expr]
+  deriving (Show, Eq)
+
+-- | Supported types
+data Type
+  = TypeInt
+  | TypeBool
+  | TypeVoid
+  deriving (Show, Eq)
+
+-- | Operators
+data Op
+  = Add | Sub | Mul | Div
+  | Eq | Neq | Lt | Gt | Le | Ge
+  | And | Or
+  deriving (Show, Eq)
+
+-- Security
+data Located a
+    = Located {
+        pos :: SourcePos,
+        value :: a
+    } deriving (Show, Eq)
