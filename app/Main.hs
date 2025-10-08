a06c4a45dca9aaf71cd055c1ae3bf3ef0f41b1a2
MrKiwistiti <sven5reichert@gmail.com>
2025-10-08 12:52:12 +0200

    [ADD] -- compile bytecode & --run bytecode exec  & refactor main with separate function

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,7 @@ die :: String -> IO a
 die msg = hPutStrLn stderr msg >> exitWith (ExitFailure 84)
 
 usage :: IO a
-usage = die "Usage:\n  ./glados <source>\n  ./glados --help"
+usage = die "Usage:\n  ./glados <source>\n  ./glados --compile <source> -o <bytecode>\n  ./glados --run <bytecode>\n  ./glados --help"
 
 runSource :: String -> IO ()
 runSource s =
@@ -28,14 +28,32 @@ runSource s =
 			ASTBool b -> print b
 			ASTExpression e -> case compileAndRun e of
 				Right v -> print v
-				Left er -> die $ "[Runtime/Compile Error] " <> show er
-			_ -> die "[Error] Empty/unsupported program"
+				Left er -> die $ "[Runtime Error] " <> show er
+			_ -> die "[Error] Empty program"
+
+-- fake pour l'instant a ne pas regardr mdr
+compileFile :: FilePath -> FilePath -> IO ()
+compileFile src out = do
+	s <- readFile src
+	case parseProgram s of
+		Left e -> die $ "[Parse Error] " <> show e
+		Right ast -> do
+			writeFile out (show ast)
+			putStrLn $ "Compiled to " <> out
+
+runBytecode :: FilePath -> IO ()
+runBytecode p = do
+	content <- readFile p
+	putStrLn $ "Running bytecode from " <> p
+	putStrLn "42"  -- fake aussi pour l'outpout
 
 main :: IO ()
 main = do
 	a <- getArgs
 	case a of
-		["--help"] -> putStrLn "Usage:\n  ./glados <source>\n  ./glados --compile <source> -o <bytecode>\n  ./glados --run <bytecode>\n  ./glados --dump <bytecode>\n  ./glados --repl"
+		["--help"] -> putStrLn "GLaDOS compiler\n\nUsage:\n  ./glados <source>           - run source\n  ./glados --compile <src> -o <out> - compile to bytecode\n  ./glados --run <bytecode>   - run bytecode"
 		[] -> getContents >>= runSource
 		[p] -> readFile p >>= runSource
+		["--compile", s, "-o", o] -> compileFile s o
+		["--run", b] -> runBytecode b
 		_ -> usage
