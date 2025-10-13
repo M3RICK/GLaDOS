88c86117c09afd06d225d156861a9043e2b454a8
dirty <aymeric.lamanda@epitech.eu>
2025-10-13 19:26:41 +0200

    [UPD] now handles FloatLit and UnOp

--- src/AST/Helpers.hs
+++ src/AST/Helpers.hs
@@ -7,8 +7,10 @@ import Text.Megaparsec.Pos (SourcePos, initialPos)
 getExprPos :: Expr -> SourcePos
 getExprPos (BoolLit (Located pos _)) = pos
 getExprPos (NumLit (Located pos _)) = pos
+getExprPos (FloatLit (Located pos _)) = pos
 getExprPos (Var (Located pos _)) = pos
-getExprPos (BinOp _ (Located pos _) _) = pos  -- Use position of left operand
+getExprPos (BinOp _ (Located pos _) _) = pos
+getExprPos (UnOp _ (Located pos _)) = pos
 getExprPos (Call (Located pos _) _) = pos
 
 -- Default position when we can't determine one
