da55b7fb9133319790046ebf7ef2f8bbcdb31240
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-14 16:36:40 +0200

    [ADD] Serialize is now added and should work

--- src/Bytecode/Serialize.hs
+++ src/Bytecode/Serialize.hs
@@ -4,8 +4,33 @@ import Data.Binary (encode, decode)
 -- encode: Takes any Binary type -> ByteString (file-ready bytes)
 -- decode: Takes ByteString -> any Binary type
 import qualified Data.ByteString.Lazy as BSL
--- ByteString.Lazy: Efficient representation of byte sequences
--- We'll use this to read/write files
+-- ByteString.Lazy: read/write files
 import IR.Types
+import Bytecode.BcTypes ()
 
--- Serialization helper functions will go here
+serializeProgram :: IRProgram -> BSL.ByteString
+serializeProgram program = encode program
+
+deserializeProgram :: BSL.ByteString -> Either String IRProgram
+deserializeProgram bytes =
+  case decodeOrFail bytes of
+    Left (_, _, err) -> Left $ "Bytecode deserialization failed: " ++ err
+    Right (_, _, program) -> Right program
+  where
+    decodeOrFail bs = case decode bs of
+      result -> Right (BSL.empty, 0, result)
+
+
+serializeProgramToStdout :: IRProgram -> IO ()
+serializeProgramToStdout program =
+  BSL.putStr (serializeProgram program)
+
+-- --run mode
+loadProgramFromFile :: FilePath -> IO (Either String IRProgram)
+loadProgramFromFile path = do
+  bytes <- BSL.readFile path
+  return $ deserializeProgram bytes
+
+saveProgramToFile :: FilePath -> IRProgram -> IO ()
+saveProgramToFile path program =
+  BSL.writeFile path (serializeProgram program)
