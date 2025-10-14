b02afcf556fce302f727f8d1250f6fb330bacbdd
dirty <aymeric.lamanda@epitech.eu>
2025-10-14 15:29:02 +0200

    [UPD] Refactored some functions to update and handle For and more

--- src/Compiler/Statement.hs
+++ src/Compiler/Statement.hs
@@ -4,67 +4,68 @@ import qualified IR.Types as IR
 import AST.AST
 import Compiler.Environment
 import Compiler.Expr
+import Security.Types (CheckEnv)
 
 -- Statement into IR instructions
-compileStatement :: FuncTable -> VarTable -> Statement -> [IR.Instruction]
-compileStatement funcTable varTable stmt = case stmt of
-  Decl _ name maybeExpr -> compileDecl funcTable varTable name maybeExpr
-  Assign name expr -> compileAssign funcTable varTable name expr
-  If cond thenBody elseBody -> compileIf funcTable varTable cond thenBody elseBody
-  While cond body -> compileWhile funcTable varTable cond body
-  For init cond update body -> compileFor funcTable varTable init cond update body
-  Return expr -> compileReturn funcTable varTable expr
-  ExprStmt expr -> compileExprStmt funcTable varTable expr
+compileStatement :: CheckEnv -> FuncTable -> VarTable -> Statement -> [IR.Instruction]
+compileStatement checkEnv funcTable varTable stmt = case stmt of
+  Decl _ name maybeExpr -> compileDecl checkEnv funcTable varTable name maybeExpr
+  Assign name expr -> compileAssign checkEnv funcTable varTable name expr
+  If cond thenBody elseBody -> compileIf checkEnv funcTable varTable cond thenBody elseBody
+  While cond body -> compileWhile checkEnv funcTable varTable cond body
+  For init cond update body -> compileFor checkEnv funcTable varTable init cond update body
+  Return expr -> compileReturn checkEnv funcTable varTable expr
+  ExprStmt expr -> compileExprStmt checkEnv funcTable varTable expr
 
 -- Variable declaration
-compileDecl :: FuncTable -> VarTable -> String -> Maybe Expr -> [IR.Instruction]
-compileDecl funcTable varTable name maybeExpr =
+compileDecl :: CheckEnv -> FuncTable -> VarTable -> String -> Maybe Expr -> [IR.Instruction]
+compileDecl checkEnv funcTable varTable name maybeExpr =
   case maybeExpr of
     Nothing -> []
-    Just expr -> compileDeclWithInit funcTable varTable name expr
+    Just expr -> compileDeclWithInit checkEnv funcTable varTable name expr
 
 -- Declaration with initialization
-compileDeclWithInit :: FuncTable -> VarTable -> String -> Expr -> [IR.Instruction]
-compileDeclWithInit funcTable varTable name expr =
-  compileExpr funcTable varTable expr ++
+compileDeclWithInit :: CheckEnv -> FuncTable -> VarTable -> String -> Expr -> [IR.Instruction]
+compileDeclWithInit checkEnv funcTable varTable name expr =
+  compileExpr checkEnv funcTable varTable expr ++
   [IR.SetLocal (lookupVar varTable name)]
 
 -- Compile assignment statement
-compileAssign :: FuncTable -> VarTable -> String -> Expr -> [IR.Instruction]
-compileAssign funcTable varTable name expr =
-  compileExpr funcTable varTable expr ++
+compileAssign :: CheckEnv -> FuncTable -> VarTable -> String -> Expr -> [IR.Instruction]
+compileAssign checkEnv funcTable varTable name expr =
+  compileExpr checkEnv funcTable varTable expr ++
   [IR.SetLocal (lookupVar varTable name)]
 
 -- | Compile if statement
-compileIf :: FuncTable -> VarTable -> Expr -> [Statement] -> Maybe [Statement] -> [IR.Instruction]
-compileIf funcTable varTable cond thenBody Nothing =
-  compileSimpleIf funcTable varTable cond thenBody
-compileIf funcTable varTable cond thenBody (Just elseBody) =
-  compileIfElse funcTable varTable cond thenBody elseBody
+compileIf :: CheckEnv -> FuncTable -> VarTable -> Expr -> [Statement] -> Maybe [Statement] -> [IR.Instruction]
+compileIf checkEnv funcTable varTable cond thenBody Nothing =
+  compileSimpleIf checkEnv funcTable varTable cond thenBody
+compileIf checkEnv funcTable varTable cond thenBody (Just elseBody) =
+  compileIfElse checkEnv funcTable varTable cond thenBody elseBody
 
 -- if (cond) { then }
-compileSimpleIf :: FuncTable -> VarTable -> Expr -> [Statement] -> [IR.Instruction]
-compileSimpleIf funcTable varTable cond thenBody =
+compileSimpleIf :: CheckEnv -> FuncTable -> VarTable -> Expr -> [Statement] -> [IR.Instruction]
+compileSimpleIf checkEnv funcTable varTable cond thenBody =
   conditionCode ++
   [IR.JumpIfFalse skipDistance] ++
   thenCode
   where
-    conditionCode = compileExpr funcTable varTable cond
-    thenCode = compileStatements funcTable varTable thenBody
+    conditionCode = compileExpr checkEnv funcTable varTable cond
+    thenCode = compileStatements checkEnv funcTable varTable thenBody
     skipDistance = length thenCode + 1
 
 -- if (cond) { then } else { else }
-compileIfElse :: FuncTable -> VarTable -> Expr -> [Statement] -> [Statement] -> [IR.Instruction]
-compileIfElse funcTable varTable cond thenBody elseBody =
+compileIfElse :: CheckEnv -> FuncTable -> VarTable -> Expr -> [Statement] -> [Statement] -> [IR.Instruction]
+compileIfElse checkEnv funcTable varTable cond thenBody elseBody =
   conditionCode ++
   [IR.JumpIfFalse jumpToElse] ++
   thenCode ++
   [IR.Jump jumpToEnd] ++
   elseCode
   where
-    conditionCode = compileExpr funcTable varTable cond
-    thenCode = compileStatements funcTable varTable thenBody
-    elseCode = compileStatements funcTable varTable elseBody
+    conditionCode = compileExpr checkEnv funcTable varTable cond
+    thenCode = compileStatements checkEnv funcTable varTable thenBody
+    elseCode = compileStatements checkEnv funcTable varTable elseBody
     jumpToElse = calculateJumpToElse thenCode
     jumpToEnd = calculateJumpToEnd elseCode
 
@@ -77,15 +78,15 @@ calculateJumpToEnd :: [IR.Instruction] -> Int
 calculateJumpToEnd elseCode = length elseCode + 1   -- rdv apres le else
 
 -- while (cond) { body }
-compileWhile :: FuncTable -> VarTable -> Expr -> [Statement] -> [IR.Instruction]
-compileWhile funcTable varTable cond body =
+compileWhile :: CheckEnv -> FuncTable -> VarTable -> Expr -> [Statement] -> [IR.Instruction]
+compileWhile checkEnv funcTable varTable cond body =
   conditionCode ++
   [IR.JumpIfFalse jumpToEnd] ++
   bodyCode ++
   [IR.Jump jumpBackToStart]
   where
-    conditionCode = compileExpr funcTable varTable cond
-    bodyCode = compileStatements funcTable varTable body
+    conditionCode = compileExpr checkEnv funcTable varTable cond
+    bodyCode = compileStatements checkEnv funcTable varTable body
     jumpToEnd = calculateJumpToLoopEnd bodyCode
     jumpBackToStart = calculateJumpBackToLoopStart conditionCode bodyCode
 
@@ -100,32 +101,32 @@ calculateJumpBackToLoopStart conditionCode bodyCode =
     totalLoopSize = length conditionCode + 1 + length bodyCode
 
 -- return statement
-compileReturn :: FuncTable -> VarTable -> Expr -> [IR.Instruction]
-compileReturn funcTable varTable expr =
-  compileExprAndReturn funcTable varTable expr
+compileReturn :: CheckEnv -> FuncTable -> VarTable -> Expr -> [IR.Instruction]
+compileReturn checkEnv funcTable varTable expr =
+  compileExprAndReturn checkEnv funcTable varTable expr
 
-compileExprAndReturn :: FuncTable -> VarTable -> Expr -> [IR.Instruction]
-compileExprAndReturn funcTable varTable expr =
-  compileExpr funcTable varTable expr ++ [IR.Return]
+compileExprAndReturn :: CheckEnv -> FuncTable -> VarTable -> Expr -> [IR.Instruction]
+compileExprAndReturn checkEnv funcTable varTable expr =
+  compileExpr checkEnv funcTable varTable expr ++ [IR.Return]
 
 -- expression statement (result discarded)
-compileExprStmt :: FuncTable -> VarTable -> Expr -> [IR.Instruction]
-compileExprStmt funcTable varTable expr =
-  compileExprAndDiscard funcTable varTable expr
+compileExprStmt :: CheckEnv -> FuncTable -> VarTable -> Expr -> [IR.Instruction]
+compileExprStmt checkEnv funcTable varTable expr =
+  compileExprAndDiscard checkEnv funcTable varTable expr
 
 -- | Compile expression and discard result (for side effects only)
-compileExprAndDiscard :: FuncTable -> VarTable -> Expr -> [IR.Instruction]
-compileExprAndDiscard funcTable varTable expr =
-  compileExpr funcTable varTable expr ++ [IR.Pop]
+compileExprAndDiscard :: CheckEnv -> FuncTable -> VarTable -> Expr -> [IR.Instruction]
+compileExprAndDiscard checkEnv funcTable varTable expr =
+  compileExpr checkEnv funcTable varTable expr ++ [IR.Pop]
 
 -- Compile multiple statements
-compileStatements :: FuncTable -> VarTable -> [Statement] -> [IR.Instruction]
-compileStatements funcTable varTable stmts =
-  concatMap (compileStatement funcTable varTable) stmts
+compileStatements :: CheckEnv -> FuncTable -> VarTable -> [Statement] -> [IR.Instruction]
+compileStatements checkEnv funcTable varTable stmts =
+  concatMap (compileStatement checkEnv funcTable varTable) stmts
 
 -- for (init; cond; update) { body }
-compileFor :: FuncTable -> VarTable -> Maybe Statement -> Maybe Expr -> Maybe Statement -> [Statement] -> [IR.Instruction]
-compileFor funcTable varTable initStmt condExpr updateStmt body =
+compileFor :: CheckEnv -> FuncTable -> VarTable -> Maybe Statement -> Maybe Expr -> Maybe Statement -> [Statement] -> [IR.Instruction]
+compileFor checkEnv funcTable varTable initStmt condExpr updateStmt body =
   initCode ++
   conditionCode ++
   [IR.JumpIfFalse jumpToEnd] ++
@@ -133,29 +134,29 @@ compileFor funcTable varTable initStmt condExpr updateStmt body =
   updateCode ++
   [IR.Jump jumpBackToStart]
   where
-    initCode = compileForInit funcTable varTable initStmt
-    conditionCode = compileForCondition funcTable varTable condExpr
-    bodyCode = compileStatements funcTable varTable body
-    updateCode = compileForUpdate funcTable varTable updateStmt
+    initCode = compileForInit checkEnv funcTable varTable initStmt
+    conditionCode = compileForCondition checkEnv funcTable varTable condExpr
+    bodyCode = compileStatements checkEnv funcTable varTable body
+    updateCode = compileForUpdate checkEnv funcTable varTable updateStmt
     jumpToEnd = calculateForLoopEnd bodyCode updateCode
     jumpBackToStart = calculateForLoopStart conditionCode bodyCode updateCode
 
 -- for loop init (runs once before loop)
-compileForInit :: FuncTable -> VarTable -> Maybe Statement -> [IR.Instruction]
-compileForInit funcTable varTable initStmt =
-  maybe [] (compileStatement funcTable varTable) initStmt
+compileForInit :: CheckEnv -> FuncTable -> VarTable -> Maybe Statement -> [IR.Instruction]
+compileForInit checkEnv funcTable varTable initStmt =
+  maybe [] (compileStatement checkEnv funcTable varTable) initStmt
 
 -- for loop condition (faut check avant chaque iteration)
-compileForCondition :: FuncTable -> VarTable -> Maybe Expr -> [IR.Instruction]
-compileForCondition funcTable varTable Nothing =
+compileForCondition :: CheckEnv -> FuncTable -> VarTable -> Maybe Expr -> [IR.Instruction]
+compileForCondition checkEnv funcTable varTable Nothing =
   [IR.PushBool True]  -- Alors infinite (void) si y a pas de conditions
-compileForCondition funcTable varTable (Just cond) =
-  compileExpr funcTable varTable cond
+compileForCondition checkEnv funcTable varTable (Just cond) =
+  compileExpr checkEnv funcTable varTable cond
 
 -- for loop update a relancer apres chaque iteration
-compileForUpdate :: FuncTable -> VarTable -> Maybe Statement -> [IR.Instruction]
-compileForUpdate funcTable varTable updateStmt =
-  maybe [] (compileStatement funcTable varTable) updateStmt
+compileForUpdate :: CheckEnv -> FuncTable -> VarTable -> Maybe Statement -> [IR.Instruction]
+compileForUpdate checkEnv funcTable varTable updateStmt =
+  maybe [] (compileStatement checkEnv funcTable varTable) updateStmt
 
 -- calcule jump pour se barrer de la for loop
 calculateForLoopEnd :: [IR.Instruction] -> [IR.Instruction] -> Int
