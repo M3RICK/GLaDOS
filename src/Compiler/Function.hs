f6e736ef7f387364c545961d47c16e9d23a6bdb1
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-16 13:19:19 +0200

    [UPD] replace error calls with Either in expression compiler

--- src/Compiler/Function.hs
+++ src/Compiler/Function.hs
@@ -6,16 +6,19 @@ import Compiler.Environment
 import Compiler.Statement
 import qualified Security.Environment as SE
 import Security.Types (CheckEnv)
+import Error.Types (CompilerError)
 
 -- single function tiouf dans l'IR
-compileFunction :: CheckEnv -> FuncTable -> Function -> CompiledFunction
+compileFunction :: CheckEnv -> FuncTable -> Function -> Either CompilerError CompiledFunction
 compileFunction checkEnv funcTable func =
-  CompiledFunction
-    { funcName = fName func
-    , paramCount = countParameters func
-    , localVarCount = countLocalVariables func
-    , code = compileFunctionBody checkEnv funcTable func
-    }
+  case compileFunctionBody checkEnv funcTable func of
+    Left err -> Left err
+    Right bodyCode -> Right $ CompiledFunction
+      { funcName = fName func
+      , paramCount = countParameters func
+      , localVarCount = countLocalVariables func
+      , code = bodyCode
+      }
 
 countParameters :: Function -> Int
 countParameters func = length (fParams func)
@@ -24,7 +27,7 @@ countLocalVariables :: Function -> Int
 countLocalVariables func = length (collectLocalDecls (fBody func))
 
 -- Function body hop ca devient des instructions
-compileFunctionBody :: CheckEnv -> FuncTable -> Function -> [Instruction]
+compileFunctionBody :: CheckEnv -> FuncTable -> Function -> Either CompilerError [Instruction]
 compileFunctionBody checkEnv funcTable func =
   compileStatements completeEnv funcTable variableTable (fBody func)
   where
@@ -38,9 +41,11 @@ addLocalToEnv (name, typ) env =
   in SE.markInitialized name envWithVar
 
 -- et la on compile pas une, pas deux mais toutes les fonctions dans le programme
-compileFunctions :: [Function] -> [CompiledFunction]
+compileFunctions :: [Function] -> Either CompilerError [CompiledFunction]
 compileFunctions funcs =
-  map compileWithEnv funcs
+  case mapM compileWithEnv funcs of
+    Left err -> Left err
+    Right compiledFuncs -> Right compiledFuncs
   where
     functionTable = makeFuncTable funcs
     funcEnv = SE.collectFunctionSignatures funcs
