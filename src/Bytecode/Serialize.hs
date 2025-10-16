1f2f5878e09161e3b081ec19353fb76c10eafcc9
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-16 14:06:00 +0200

    [UPD] use try/catch for I/O since outside programs logic

--- src/Bytecode/Serialize.hs
+++ src/Bytecode/Serialize.hs
@@ -13,24 +13,29 @@ import qualified Data.ByteString.Lazy as BSL
 -- ByteString.Lazy: read/write files
 import IR.Types
 import Bytecode.BcTypes ()
+import Control.Exception (try, IOException)
 
 serializeProgram :: IRProgram -> BSL.ByteString
 serializeProgram program = encode program
 
-deserializeProgram :: BSL.ByteString -> IRProgram
-deserializeProgram bytes = decode bytes
-
+deserializeProgram :: BSL.ByteString -> Either String IRProgram
+deserializeProgram bytes = Right (decode bytes)
 
 serializeProgramToStdout :: IRProgram -> IO ()
 serializeProgramToStdout program =
     BSL.putStr (serializeProgram program)
 
 -- --run mode
-loadProgramFromFile :: FilePath -> IO IRProgram
+loadProgramFromFile :: FilePath -> IO (Either String IRProgram)
 loadProgramFromFile path = do
-    bytes <- BSL.readFile path
-    return $ deserializeProgram bytes
+    result <- try (BSL.readFile path)
+    case result of
+        Left err -> return $ Left $ "Failed to read file '" ++ path ++ "': " ++ show (err :: IOException)
+        Right bytes -> return $ deserializeProgram bytes
 
-saveProgramToFile :: FilePath -> IRProgram -> IO ()
-saveProgramToFile path program =
-    BSL.writeFile path (serializeProgram program)
+saveProgramToFile :: FilePath -> IRProgram -> IO (Either String ())
+saveProgramToFile path program = do
+    result <- try (BSL.writeFile path (serializeProgram program))
+    case result of
+        Left err -> return $ Left $ "Failed to write file '" ++ path ++ "': " ++ show (err :: IOException)
+        Right () -> return $ Right ()
