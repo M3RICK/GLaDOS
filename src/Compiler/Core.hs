d5590d5ea933a760a87cfb69cf3ca9a4a4e3fd73
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-10-16 13:22:18 +0200

    [UPD] update compiler core to propagate Either errors

--- src/Compiler/Core.hs
+++ src/Compiler/Core.hs
@@ -5,19 +5,25 @@ import IR.TextFormat
 import AST.AST
 import Compiler.Function
 import Compiler.Environment
+import Error.Types (CompilerError)
 
 -- AST -> IR
-compileProgram :: Program -> IRProgram
+compileProgram :: Program -> Either CompilerError IRProgram
 compileProgram (Program funcs) =
-  IRProgram
-    { functions = compileFunctions funcs
-    , mainIndex = findMainIndex funcs
-    }
+  case compileFunctions funcs of
+    Left err -> Left err
+    Right compiledFuncs -> Right $ IRProgram
+      { functions = compiledFuncs
+      , mainIndex = findMainIndex funcs
+      }
 
 -- Badabing badaboom ca compile ca convertit en qqch de lisible
-compileToText :: Program -> String
-compileToText program = showProgram (compileProgram program)
+compileToText :: Program -> Either CompilerError String
+compileToText program =
+  case compileProgram program of
+    Left err -> Left err
+    Right irProgram -> Right (showProgram irProgram)
 
 -- Compile to IR only (no I/O)
-compileToIR :: Program -> IRProgram
+compileToIR :: Program -> Either CompilerError IRProgram
 compileToIR = compileProgram
