3d05acadfb731331c07c59e2fca85aa17e902c93
dirty <aymeric.lamanda@epitech.eu>
2025-10-28 13:54:14 +0100

    [ADD] new test file: test_simple_loop.c

---
+++ tests/dotC/test_simple_loop.c
@@ -0,0 +1,11 @@
+int main() {
+    int sum = 0;
+    int i = 1;
+    while (i <= 5) {
+        sum = sum + i;
+        i = i + 1;
+    }
+    return sum;  // 1 + 2 + 3 + 4 + 5 = 15
+}
+
+/* Expected: Success - returns 15 */
