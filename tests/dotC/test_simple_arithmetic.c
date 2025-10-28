abb3b11cc61e83d6070a5fda189168fef977b545
dirty <aymeric.lamanda@epitech.eu>
2025-10-28 13:54:14 +0100

    [ADD] new test file: test_simple_arithmetic.c

---
+++ tests/dotC/test_simple_arithmetic.c
@@ -0,0 +1,11 @@
+int main() {
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    int diff = a - b;
+    int prod = a * b;
+    int quot = a / b;
+    return sum + diff + prod + quot;  // 15 + 5 + 50 + 2 = 72
+}
+
+/* Expected: Success - returns 72 */
