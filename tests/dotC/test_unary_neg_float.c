ec779480bfcbcc0c234fe391921042ae93ef5b81
dirty <aymeric.lamanda@epitech.eu>
2025-10-28 13:54:14 +0100

    [ADD] new test file: test_unary_neg_float.c

---
+++ tests/dotC/test_unary_neg_float.c
@@ -0,0 +1,8 @@
+// Test unary negation with floats
+float main() {
+    float x = 3.14;
+    float result = -x + 10.0;  // Should be -3.14 + 10.0 = 6.86
+    return result;
+}
+
+/* Expected: Success - returns 6.86 */
