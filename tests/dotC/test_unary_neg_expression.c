e22a51c02f5b57a949e5f9f1806387164240945c
dirty <aymeric.lamanda@epitech.eu>
2025-10-28 13:54:14 +0100

    [ADD] new test file: test_unary_neg_expression.c

---
+++ tests/dotC/test_unary_neg_expression.c
@@ -0,0 +1,8 @@
+// Test unary negation in expressions
+int main() {
+    int x = 10;
+    int result = -x + 5;  // Should be -10 + 5 = -5
+    return result;
+}
+
+/* Expected: Success - returns -5 */
